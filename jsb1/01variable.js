b77683cd7cb1eebde6da8a697ebda2d4c07197d2
jatin9099 <jatinkishor9099@gmail.com>
2025-02-07 20:43:53 +0000

    datatypes and var

---
+++ jsb1/01variable.js
@@ -0,0 +1,10 @@
+const acc_id=1234
+let acc_email="jatin@gmail.com"
+var acc_pswd="12345"
+acc_city="jaipur"
+acc_city="patna"
+let acc_state
+//acc_id = 2
+//prefer not to use var
+console.log(acc_id);
+console.table([acc_id,acc_email,acc_pswd,acc_city,acc_state])
