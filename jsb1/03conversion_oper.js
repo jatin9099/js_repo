d24f2e1358cbe5320618e9a9259198a7c034d7e5
jatin9099 <jatinkishor9099@gmail.com>
2025-02-07 21:32:34 +0000

    conversion of datatype

---
+++ jsb1/03conversion_oper.js
@@ -0,0 +1,15 @@
+let score="33a"
+console.log(typeof(score));
+let s=333
+console.log(typeof s);
+let val_in_num=Number(s)
+ console.log(val_in_num);
+console.log(typeof(val_in_num));
+
+//conversion of boolean into number
+let a=1;
+let b=Boolean(a);
+console.log(b);
+
+
+
