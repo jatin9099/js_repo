9c6d81eb790bb829e879768e0158f45912d19aa0
jatin9099 <jatinkishor9099@gmail.com>
2025-02-08 22:43:55 +0000

    array basic

--- 02_basics_JS/01_array.js
+++ 02_basics_JS/01_array.js
@@ -4,4 +4,30 @@ console.log(arr);
 const arr1=[2,4,5,7,8]
 console.log(arr1);
 console.log(arr1[3]);
+arr1.push(9)
+console.log(arr1);
+arr1.pop()
+console.log(arr1);
+arr1.unshift(9)
+console.log(arr1);
+arr1.shift()
+console.log(arr1);
+ const arr2=arr1.join()
+
+console.log(arr1);
+console.log(arr2);
+
+//slice,splice
+
+console.log(arr1.slice(1,3));//print 1-2
+console.log(arr1);
+
+console.log(arr1.splice(1,3));//print 1-2-3
+
+console.log(arr1);
+
+
+
+
+
 
