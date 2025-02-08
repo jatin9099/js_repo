0ac6c46f95f55f5b754bddeb2b9685430ed17d43
jatin9099 <jatinkishor9099@gmail.com>
2025-02-08 19:31:02 +0000

    datatype summary

---
+++ jsb1/datatype_summary.js
@@ -0,0 +1,45 @@
+// primitive
+
+// 7 types : String, Number,Boolean , null, undefined , symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const inlog =false
+const outsideTemp=null
+let userEmail;
+
+const id = Symbol('123')
+const ida = Symbol('123')
+ console.log(id==ida);
+
+ const big_no=24223444443343224234n
+ console.log(big_no);
+
+//reference (non primitive)
+ 
+// array , object , function
+
+const heros = [1,2,3,4,5]
+let my_obj = {
+    name:"jatin",
+    age:22
+}
+
+const my_function = function(){
+    console.log(hello);
+    
+}
+console.log(heros);
+console.log(my_obj);
+console.log ( typeof outsideTemp);
+
+
+
+
+
+
+ 
+ 
+
+
