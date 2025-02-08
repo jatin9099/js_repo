b99ee77d045da49d4751c3b21293050ea1c652c9
jatin9099 <jatinkishor9099@gmail.com>
2025-02-08 22:15:23 +0000

    date n time

---
+++ jsb1/07_dates_in_js.js
@@ -0,0 +1,19 @@
+let my_date=new Date()
+console.log(my_date);
+console.log(my_date.toString());
+console.log(my_date.toLocaleString());
+
+// let myd=new Date(2023, 10, 23);
+// let myd=new Date("2023-12-31");
+let myd=new Date("06-19-2023");
+
+
+console.log(myd.toDateString());
+
+let mytimestamp=Date.now()
+console.log(mytimestamp);
+
+console.log(Math.floor(Date.now()/1000));
+
+
+
