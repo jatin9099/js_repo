0c2b564fd6f546e245cc6a270c9ef6b22f836a15
jatin9099 <jatinkishor9099@gmail.com>
2025-02-08 10:34:08 +0000

    conversion of datatype

--- jsb1/03conversion_oper.js
+++ jsb1/03conversion_oper.js
@@ -10,6 +10,28 @@ console.log(typeof(val_in_num));
 let a=1;
 let b=Boolean(a);
 console.log(b);
+//bigint
+const big_no = 12323423456553323456432434n
+console.log(big_no);
+
+let aa=""
+let bb=Boolean(aa)
+console.log(bb);
+
+
+//array , object , function
+const heros=["shaktiman","naagraj","doga"];
+
+let myobj={
+    name:"jatin",
+     age:22
+}
+
+function hello(){
+    console.log("hello");  
+}
+console.log(typeof big_no);
+
 
 
 
