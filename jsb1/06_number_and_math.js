da1dc2f911320e5b9aaf0c8d805157cd3ce76c42
jatin9099 <jatinkishor9099@gmail.com>
2025-02-08 21:27:42 +0000

    number and math

---
+++ jsb1/06_number_and_math.js
@@ -0,0 +1,25 @@
+const score =400
+const balance = new Number(100)
+console.log(score);
+console.log(balance);
+
+const sc=1236.8954
+console.log(sc.toPrecision(4));
+
+const a=10000000
+console.log(a.toLocaleString());
+console.log(a.toLocaleString('en-IN'));
+
+
+//************ Maths *********/
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
