ddac0750e4ba9d24ea02002fbf799fda86e15f4f
jatin9099 <jatinkishor9099@gmail.com>
2025-02-10 20:03:43 +0000

    iteration_JS

---
+++ 05_iteration_JS/01_one.js
@@ -0,0 +1,36 @@
+//for
+
+for(let i=0 ; i<5 ; i++)
+{
+    console.log(i);
+    
+}
+
+
+for(let i=0 ; i<3 ; i++)
+{
+    for(let j=0 ; j<5 ; j++)
+    {
+        console.log(`inner ${i} outer ${j}`);
+        
+    }
+}
+
+let arr=['a','b','c']
+
+for(let i=0 ; i<arr.length ; i++)
+{
+    console.log(arr[i]);
+    
+}
+
+// break and continue
+
+//while and do while 
+let i=0
+while(i<10)
+{
+    console.log(i);
+    i++
+    
+}
